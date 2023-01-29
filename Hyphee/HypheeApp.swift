89f30bf8a093fc1b65e3301e995bc87ddd5620bc
Josiah Green <JosiahGreen90@gmail.com>
2023-01-28 22:16:27 -0800

    Initial Commit

---
+++ Hyphee/HypheeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypheeApp.swift
+//  Hyphee
+//
+//  Created by Josiah Green on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HypheeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
